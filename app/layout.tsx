9a6ee647640f665ba9371bad4940fb4a915f7600
boris dimitrijevic <borisdimitrijevicit@gmail.com>
2024-06-24 13:52:13 +0200

    Added assets and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,34 @@ import {
   // UserButton,
 } from '@clerk/nextjs';
 
+import { Inter, Space_Grotesk } from 'next/font/google';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dev Overflow',
+  description:
+    'a communit ydrivern platform for asking and answering programming questions!',
+  icons: {
+    icon: '/asssets/images/site-logo.svg',
+  },
+};
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--inter',
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--spaceGrotesk',
+});
+
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
@@ -30,4 +53,3 @@ const layout = ({ children }: { children: React.ReactNode }) => {
 };
 
 export default layout;
-// const inter = Inter({ subsets: ['latin'] });
