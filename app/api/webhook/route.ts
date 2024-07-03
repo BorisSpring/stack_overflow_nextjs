933b59d683b6c0aac5a59f05d75749cbfc31fa84
Boris Dimitrijevic <borisdimitrijevicit@gmail.com>
2024-07-03 16:42:28 +0200

    Update route.ts

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -86,6 +86,7 @@ export async function POST(req: Request) {
   }
 
   if (eventType === 'user.deleted') {
+     console.log({data: evt.data})
     const { id } = evt.data;
     const deletedUser = await deleteUser({ clerkId: id! });
 
